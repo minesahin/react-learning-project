fa946b81c624fa06aa42a90abf2da7a0507a7590
minesahin <sahimine1@gmail.com>
2023-09-17 19:58:23 +0300

    delete and form open/close

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import TodoTable from "./components/TodoTable";
 import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
+
+  const [showAddTodoForm,setShowAddTodoForm]= useState(false);
+
   const [todos, setTodos] = useState([
     { rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "User One" },
     { rowNumber: 2, rowDescription: "Water plants", rowAssigned: "User Two" },
@@ -15,15 +18,25 @@ function App() {
     },
   ]);
 
-  const addTodo = (description,assigned) => {
+  const addTodo = (description, assigned) => {
+    let rowNumber = 0;
     if (todos.length > 0) {
-      const newTodo = {
-        rowNumber: todos.length + 1,
-        rowDescription: description,
-        rowAssigned: assigned,
-      };
-      setTodos((todos) => [...todos, newTodo]);
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
+      rowNumber = 1;
     }
+    const newTodo = {
+      rowNumber: rowNumber,
+      rowDescription: description,
+      rowAssigned: assigned,
+    };
+    setTodos((todos) => [...todos, newTodo]);
+  };
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+    setTodos(filtered);
   };
 
   return (
@@ -31,11 +44,14 @@ function App() {
       <div class="card">
         <div class="card-header">Your Todo's</div>
         <div class="card-body">
-          <TodoTable todos={todos} />
-          <button className="btn btn-primary" onClick={addTodo}>
-            Add new todo
+          <TodoTable todos={todos} deleteTodo={deleteTodo} />
+          <button onClick={()=>setShowAddTodoForm(!showAddTodoForm)} className="btn btn-primary">
+          {showAddTodoForm ? 'Close New Todo': 'New Todo'}
           </button>
-          <NewTodoForm addTodo={addTodo}/> 
+          
+          {showAddTodoForm &&
+          <NewTodoForm addTodo={addTodo}/>}
+         
         </div>
       </div>
     </div>
